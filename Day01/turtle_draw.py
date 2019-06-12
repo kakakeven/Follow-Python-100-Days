1ddffa857f75f090ccdc2c4640b4daaa1f1e8007
kakakeven <gaoyt2012@foxmail.com>
2019-06-12 15:40:55 +0800

    Day 1
    1 了解 Python 历史
    2 安装 Python

---
+++ Day01/turtle_draw.py
@@ -0,0 +1,17 @@
+'''
+学习使用 turtle 在屏幕上绘制图形
+
+@author kakakeven
+'''
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
