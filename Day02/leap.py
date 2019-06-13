35a1c2d025899f2322aa791af8e9b0d5df072304
kakakeven <gaoyt2012@foxmail.com>
2019-06-13 18:13:05 +0800

    完善闰年计算&删除 TODO
    根据维基百科定义完善闰年计算；
    删除 TODO

--- Day02/leap.py
+++ Day02/leap.py
@@ -4,5 +4,5 @@
 @author: kakakeven
 """
 year = int(input("请输入年份: "))
-is_leap = (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
+is_leap = ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0 and year % 3200 != 0))
 print(is_leap)
